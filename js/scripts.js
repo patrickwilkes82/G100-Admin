b145da8f07c2a455d8aca1f6225940ab75787289
patrickwilkes82 <patwilkes82@gmail.com>
2019-10-07 17:47:50 -0400

    added javascript file

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+//Toggle SideMenu Bar Class when clicking Arrow Button
+function toggleMenu() {
+    let menuSize = document.getElementById('wrapper');
+    menuSize.classList.toggle('active');
+    console.log('class toggled');
+}
