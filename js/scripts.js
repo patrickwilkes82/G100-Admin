eb3568d2e4e0bd061c6649fb3ef2abd8ba514984
patrickwilkes82 <patwilkes82@gmail.com>
2019-10-09 15:52:40 -0400

    js scripts added to open & close menus

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 //Toggle SideMenu Bar Class when clicking Arrow Button
+
 function toggleMenu() {
-    let menuSize = document.getElementById('wrapper');
+    const menuSize = document.getElementById('wrapper');
     menuSize.classList.toggle('active');
     console.log('class toggled');
+}
+
+function toggleConfig() {
+    const showConfigMenu = document.getElementById('config_menu');
+    showConfigMenu.classList.toggle('open');
+    console.log('config menu toggled');
+}
+
+function closeConfigMenu() {
+    const closeConfigMenu = document.getElementById('config_menu');
+    closeConfigMenu.classList.remove('open');
+    console.log('all menus closed hopefully');
 }
