5115d85bb8b5cc98152f3cddc6a63fd290073c04
patrickwilkes82 <patwilkes82@gmail.com>
2019-10-14 17:02:53 -0400

    added notifications menu and esc key menu close

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,41 @@ function toggleMenu() {
 function toggleConfig() {
     const showConfigMenu = document.getElementById('config_menu');
     let configButton = document.getElementById('configButton');
-    let wrapperOpacity = document.getElementById('wrapper');
+
     showConfigMenu.classList.toggle('open');
     configButton.classList.toggle ('open');
+    closeNotifications();
 }
 
 function closeConfigMenu() {
     const closeConfigMenu = document.getElementById('config_menu');
     let configButton = document.getElementById('configButton');
+
     closeConfigMenu.classList.remove('open');
     configButton.classList.remove('open');
 }
+
+function toggleNotifications() {
+    const showNotificationMenu = document.getElementById('notification_menu');
+    let notificationButton = document.getElementById('notificationButton');
+
+    showNotificationMenu.classList.toggle('open');
+    notificationButton.classList.toggle('open');
+    closeConfigMenu();
+}
+
+function closeNotifications() {
+    const closeNotificationsMenu = document.getElementById('notification_menu');
+    let notificationButton = document.getElementById('notificationButton');
+
+    closeNotificationsMenu.classList.remove('open');
+    notificationButton.classList.remove('open');
+}
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        closeConfigMenu();
+        closeNotifications();
+    }
+};
